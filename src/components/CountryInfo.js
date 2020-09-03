fd32701aba36c8856d476bbcb79876b1cb9581a3
Manuella Vieira <manuellavr7@gmail.com>
2020-09-03 19:49:24 -0300

    minor changes

--- src/components/CountryInfo.js
+++ src/components/CountryInfo.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 export default function CountryInfo(props){
 
 	const country = props.location.country
+	const url = `https://en.wikipedia.org/wiki/${country.name.replace(/ /g, "_")}`
 
 	return(
 		<>
@@ -15,16 +16,19 @@ export default function CountryInfo(props){
 			{country.name === country.native ? country.name : `${country.name} (${country.native})`}
 		</h1>
 		</div>
+		<div className="row"><p className="subtext">Learn more at: <a href={url} target="__blank">https://en.wikipedia.org/wiki/{country.name.replace(/ /g, "_")}</a></p>
+
+</div>
 		<div className="info_container">
 		<div className="row">
 			<div className="col-sm-6 info">
-				<div className="big_txt"><b>Phone:</b> {country.phone}</div>
-				<div className="big_txt"><b>Capital:</b> {country.capital}</div>
-				<div className="big_txt"><b>Languages:</b> {country.languages.map(lang => lang.name).join(", ")}</div>
+				<div className="big_txt"><i class="fa fa-phone" aria-hidden="true"></i> <b>Phone:</b> {country.phone}</div>
+				<div className="big_txt"><i class="fa fa-building-o" aria-hidden="true"></i> <b>Capital:</b> {country.capital}</div>
+				<div className="big_txt"><i class="fa fa-language" aria-hidden="true"></i> <b>Language(s):</b> {country.languages.map(lang => lang.name).join(", ")}</div>
 			</div>
 			<div className="col-sm-6 info">
-				<div className="big_txt"><b>Continent:</b> {country.continent.name} </div>
-				<div className="big_txt"><b>Currency:</b> {country.currency}</div>
+				<div className="big_txt"><i class="fa fa-globe" aria-hidden="true"></i> <b>Continent:</b> {country.continent.name} </div>
+				<div className="big_txt"><i class="fa fa-money" aria-hidden="true"></i> <b>Currency:</b> {country.currency}</div>
 				<div className="big_txt"><b>{country.states.length > 0 ? "States:" : ""}</b></div>
 				<p className="medium_txt">{ country.states.length > 0 ?  country.states.map(state => state.name).join(", ") : ""}</p>
 			</div>
